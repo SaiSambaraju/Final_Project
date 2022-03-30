690af23b36b6fbfeb8b9c4c901d34e957fe75700
Sai Sambaraju <sai.sambaraju@student.manchester.ac.uk>
2022-03-30 21:06:03 +0100

    Added some logic for ultrasonic, tested all other components work together, but need to integrate ultrasonic with pump logic

--- greenhouse/greenhouse1/greenhouse1.ino
+++ greenhouse/greenhouse1/greenhouse1.ino
@@ -1,16 +1,18 @@
 #define analogRef_voltage 3.3
 
-//global varaibles
+//global variables
 
 //sensor read pins
 const int ldrpin = A3;
 const int moisturesensor = A4;
 const int tempPin = A0;
+const int ultra_sens_trig = 7;
+const int ultra_sens_echo = 8;
 
 //effector pins
 const int ledstrip = 6;
 const int fanpin = 3;
-const int pumppin = 11;
+const int pumppin = 5;
 
 //dryest and wettest val of moisture sensor
 const int dryest_val = 775;
@@ -27,11 +29,18 @@ int temperature = 0;
 int fan_same_temp_counter = 0;
 int moisture_value;
 int moisture_percent;
+
+//control variables for pump
 bool pump_on = false;
+bool pump_needs_on = false;
 
 //desired conditions
 const int desired_temp = 25; 
 const int desired_moisture_percent = 30;
+
+//moisture threshold variable
+const int moisture_low = 15; 
+
 
 //calibrate function
 void calibrate(){
@@ -60,57 +69,77 @@ int get_moisture_perc(int moisture_raw){
 }
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(fanpin, OUTPUT);
   pinMode(pumppin, OUTPUT);
   pinMode(ledstrip, OUTPUT);
-//  calibrate();  //calibrate
-//  Serial.begin(9600);
-//  analogReference(EXTERNAL);
-//  noInterrupts();
-//  TCCR1A = 0;
-//  TCCR1B = 0;
-//  TCNT1 = 0;
-//  OCR1A = 62500;
-//  TCCR1B |= (1 << WGM12);
-//  TCCR1B |= (1 << CS12) | (1 << CS10);
-//  TIMSK1 |= (1 << OCIE1A);
-//  interrupts();
+  pinMode(ultra_sens_trig, OUTPUT);
+  pinMode(ultra_sens_echo, INPUT);
+  calibrate();  //calibrate
+  Serial.begin(9600);
+  analogReference(EXTERNAL);
+  noInterrupts();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+  OCR1A = 62500;
+  TCCR1B |= (1 << WGM12);
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+  TIMSK1 |= (1 << OCIE1A);
+  interrupts();
 }
 
 ISR(TIMER1_COMPA_vect){
   Serial.println("Hello this is an interrupt!!");
-  //Control system 1
-  ldrvalue = analogRead(ldrpin);
-  ldrvalue = map(constrain(ldrvalue, ldrmin_value, ldrmax_value), ldrmin_value, ldrmax_value, 255, 0);
-  analogWrite(ledstrip, ldrvalue);
   
-  //Control System 2
-  temperature = get_temp_C(analogRead(tempPin));
-  Serial.println(temperature);
-  if(temperature > desired_temp and fan_same_temp_counter < 5){
-    analogWrite(fanpin, 150);
-    fan_same_temp_counter += 1;
-  }
-  else if(temperature > desired_temp and fan_same_temp_counter >= 5){
-    Serial.println("Fan is at 255");
-    analogWrite(fanpin, 255);
-  }
-  else{
-    fan_same_temp_counter = 0;
-    digitalWrite(fanpin, LOW);
-  }
+  //Control system 1
+//  ldrvalue = analogRead(ldrpin);
+//  ldrvalue = map(constrain(ldrvalue, ldrmin_value, ldrmax_value), ldrmin_value, ldrmax_value, 255, 0);
+//  analogWrite(ledstrip, ldrvalue);
+//  
+//  //Control System 2
+//  temperature = get_temp_C(analogRead(tempPin));
+//  Serial.println(temperature);
+//  if(temperature > desired_temp and fan_same_temp_counter < 5){
+//    analogWrite(fanpin, 150);
+//    fan_same_temp_counter += 1;
+//  }
+//  else if(temperature > desired_temp and fan_same_temp_counter >= 5){
+//    Serial.println("Fan is at 255");
+//    analogWrite(fanpin, 255);
+//  }
+//  else{
+//    fan_same_temp_counter = 0;
+//    analogWrite(fanpin, 0);
+//  }
   
   //Control system 3
 //  moisture_percent = get_moisture_perc(analogRead(moisturesensor));
-  Serial.println(moisture_percent);
-//  if(moisture_percent < desired_moisture_percent and pump_on == false){
-//    analogWrite(pumppin, 200);
-//    pump_on = true;
+//  Serial.println(moisture_percent);
+    digitalWrite(ultra_sens_trig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(ultra_sens_trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(ultra_sens_trig, LOW);
+    long duration = pulseIn(ultra_sens_echo, HIGH);
+    long distance = duration/58.2;
+    Serial.println(distance);
+//  if(moisture_percent <= moisture_low){
+//      pump_needs_on = true;  
 //  }
-//  else{
-//    analogWrite(pumppin, 0);
-//    pump_on = false;
+//  else if(moisture_percent >= desired_moisture_percent){
+//      pump_needs_on = false;
+//      pump_on = false;
+//  }
+//  if(pump_needs_on){
+//   if(pump_on == false){
+//      Serial.println("pump turning on");
+//      analogWrite(pumppin, 200);
+//      pump_on = true;
+//    }
+//    else{
+//      analogWrite(pumppin, 0);
+//      pump_on = false;
+//    }
 //  }
 }
 
